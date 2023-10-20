eaea2f343c5def8116888e115bc7ff28e6a779cd
Doruk Aytekin <horationelson1903@gmail.com>
2023-10-20 14:28:21 -0700

    add solution 2

---
+++ probality_calculator.py
@@ -0,0 +1,50 @@
+import random
+class Hat:
+    contents = []
+    drawlist = []
+    def __init__(self,**kwargs):
+        self.contents.clear()
+        self.drawlist.clear()
+        for key, value in kwargs.items():
+            for i in range(value):
+                self.contents.append(key)
+    def draw(self,repeat):
+        for i in range(repeat):
+            number = random.choice(self.contents)
+            self.drawlist.append(number)
+            self.contents.remove(number)
+        return self.drawlist
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count = 0
+    result = []
+    wanted_balss = []
+    for key,value in expected_balls.items():
+        for i in range(value):
+            wanted_balss.append(key)
+    for i in range(num_experiments):
+        result = hat.draw(num_balls_drawn)
+        check = all(item in result for item in wanted_balss)
+        print(expected_balls)
+        if(check):
+            count += 1
+            hat.contents += hat.drawlist
+            hat.drawlist.clear()
+            continue
+            
+        else:
+            hat.contents += hat.drawlist
+            hat.drawlist.clear()
+            continue
+    print(count)
+    return count / num_experiments
+
+# hat = Hat(red=3, blue=2)
+# hat = Hat(red=5, blue=2)
+# actual = hat.contents
+# print(actual)
+hat = Hat(yellow=5, red=1, green=3, blue=9, test=1)
+probability = experiment(hat=hat,
+                                             expected_balls={"yellow":2,"blue":4,"test":1},
+                                             num_balls_drawn=19,
+                                             num_experiments=100)
+print(probability)
